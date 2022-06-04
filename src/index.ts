baca7235aff389612bb730222910c2906be5bb80
Mohit-S-Rane <mohitsrane16@gmail.com>
2022-06-04 16:26:00 +0530

    Basics of Middleware

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,13 @@ let app: express.Application = express();
 app.listen(5000, () => {
   console.log("Server is running at port 5000");
 });
- 
+
+app.get("/api/user/login", (req, res) => {
+  const data = [{ name: "testUserName" }];
+  res.status(200).send(data);
+});
+
+app.get("/api/user/test", (req, res) => {
+  const data = [{ name: "testUserName" }];
+  res.status(200).send(data);
+});
