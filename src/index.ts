9b4307d1ff6dacb72b58bd77a48f197ca2f09919
Mohit-S-Rane <mohitsrane16@gmail.com>
2022-06-07 11:44:38 +0530

    Connecting to MongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import * as express from "express";
+import * as mongoose from "mongoose";
 
 let app: express.Application = express();
 
 app.listen(5000, () => {
   console.log("Server is running at port 5000");
+});
+
+mongoose.connect('mongodb+srv://admin:admin@cluster101.eyren.mongodb.net/?retryWrites=true&w=majority')
+.then(()=> {
+  console.log('MongoDB is now connected');
 });
 
 app.use(function (req, res, next) {
